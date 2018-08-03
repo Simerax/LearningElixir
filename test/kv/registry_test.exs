6a33f14fc85299c4748b41bee4079efc4ed23700
Simerax <xaverfischer@mail.de>
2018-08-03 23:50:53 +0200

    inital commit

---
+++ test/kv/registry_test.exs
@@ -0,0 +1,26 @@
+defmodule KV.RegistyTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    registry = start_supervised!(KV.Registry)
+    %{registry: registry}
+  end
+
+  test "spawn Buckets", %{registry: registry} do
+    assert KV.Registry.lookup(registry, "shopping") == :error
+
+    KV.Registry.create(registry, "shopping")
+    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    KV.Bucket.put(bucket, "IceCream", 5)
+    assert KV.Bucket.get(bucket, "IceCream") == 5
+  end
+
+  test "delete Bucket on Running Server", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    # Kill the Bucket process
+    Agent.stop(bucket)
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+end
